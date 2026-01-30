f77c2b72b2ae0c1f33ee5323a07b31365e021f6c
Maria Luiza <marialluantunes@gmail.com>
2026-01-30 19:29:48 -0300

    Cria teste para verificar a inserção na lista Cliente

---
+++ testes_cliente.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "cliente.h"
+
+//------------------------------- TESTE DE INSERCAO ----------------------------------------
+
+int main() {
+    
+    Cliente *cabeça = criarHeadCliente();
+
+    char cpf_temp[12];
+    char nome_temp[100];
+    char email_temp[100];
+    char telefone_temp[15];
+
+    printf("--------CADASTRO CLIENTE-------\n");
+    printf("Digite o cpf: ");
+    scanf(" %11s", cpf_temp); 
+
+
+    printf("\nDigite o nome: ");
+    scanf(" %99[^\n]" , nome_temp);
+
+
+    printf("\nDigite o email: ");
+    scanf(" %99s" , email_temp);
+
+
+    printf("\nDigite o telefone: ");
+    scanf(" %14s" , telefone_temp);
+
+
+    inserirClienteInicio(cabeça , cpf_temp, nome_temp, email_temp, telefone_temp);
+
+    Cliente *c = cabeça->next;
+
+    printf("\n--- CLIENTE NA LISTA ---\n");
+    printf("CPF: %s\n", c->cpf);
+    printf("Nome: %s\n", c->nome);
+    printf("Email: %s\n", c->email);
+    printf("Telefone: %s\n", c->telefone);
+
+
+
+}
