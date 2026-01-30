ba8253509b90193fbf27131c5264ef13d8777085
Maria Luiza <marialluantunes@gmail.com>
2026-01-30 02:14:15 -0300

    adiciona struct Cliente

--- cliente.h
+++ cliente.h
@@ -1,5 +1,37 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
 
+//----------- STRUCT ------------------
+
+typedef struct Cliente {
+    char *cpf;
+    char *nome;
+    char *email;
+    char *telefone;
+
+    struct Cliente *next;
+
+} Cliente;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif
